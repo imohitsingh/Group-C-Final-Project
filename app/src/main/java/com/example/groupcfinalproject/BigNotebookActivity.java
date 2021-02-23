817376f30e80ef7302277f35385d3539de6bdca3
Mohit Singh <mohit26998@gmail.com>
2021-02-22 20:17:04 -0800

    added background colour and added three buttons to this activity!

--- app/src/main/java/com/example/groupcfinalproject/BigNotebookActivity.java
+++ app/src/main/java/com/example/groupcfinalproject/BigNotebookActivity.java
@@ -2,7 +2,9 @@ package com.example.groupcfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BigNotebookActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class BigNotebookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_big_notebook);
+    }
+
+    public void goTo_SmallNotebookActivity(View view) {
+        Intent smallnotebook_intent = new Intent(this, SmallNotebookActivity.class);
+        startActivity(smallnotebook_intent);
     }
 }
