ea2c492bf3d1ef5bc2830ad0e12f1f83f37f9544
John Rey <42550367+SirPancakes@users.noreply.github.com>
2021-02-22 18:44:27 -0800

    created base app

---
+++ app/src/main/java/com/example/groupcfinalproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.groupcfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goTo_SmallNotebookActivity(View view) {
+        Intent smallnotebook_intent = new Intent(this, SmallNotebookActivity.class);
+        startActivity(smallnotebook_intent);
+    }
+
+    public void goTo_BigNotebookActivity(View view) {
+        Intent bignotebook_intent = new Intent(this, BigNotebookActivity.class);
+        startActivity(bignotebook_intent);
+    }
+}
---
+++ app/src/main/java/com/example/groupcfinalproject/SmallNotebookActivity.java
@@ -0,0 +1,14 @@
+package com.example.groupcfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SmallNotebookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_small_notebook);
+    }
+}
