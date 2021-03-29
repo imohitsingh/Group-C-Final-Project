741a1334c1af5a010fced97f5e690077e5986b83
John Rey <42550367+SirPancakes@users.noreply.github.com>
2021-03-29 15:17:44 -0700

    March 29, 2021 SmallNotebookActivity -Fixed constraints -Fixed Layout

--- app/src/main/java/com/example/groupcfinalproject/Settings.java
+++ app/src/main/java/com/example/groupcfinalproject/Settings.java
@@ -2,7 +2,12 @@ package com.example.groupcfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
     }
+
+    public class activity_settings extends Activity implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            // An item was selected. You can retrieve the selected item using
+            // parent.getItemAtPosition(pos)
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
+    }
+    
+
 }
